f8e6cc7f9ffc5406d22a501fb71ba71e8fa334f4
Michael Aroworade <Ayoaro85@gmail.com>
2024-01-27 06:30:30 +0100

    First commit

---
+++ c_syntax/syntax.c
@@ -0,0 +1,16 @@
+/*
+ * File: syntax.c
+ */
+#include <stdio.h> /* header file library */
+
+/**
+ * main - print Hello World to screen
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("Hello World!"); /* printf() is a function used to print to screen */
+
+	return (0);
+}
